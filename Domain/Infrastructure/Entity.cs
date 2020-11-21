fbe4c819d34521c45a1cf5fb710440d7ebbdada3
Stepanov Mikhail <mihanoid10@yandex.ru>
2020-11-21 20:31:11 +0500

    Add basic infrastructure

---
+++ Domain/Infrastructure/Entity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Infrastructure
+{
+    /// <summary>
+    /// Базовый класс DDD сущностей
+    /// </summary>
+    /// <typeparam name="TId">Тип идентификатора</typeparam>
+    public abstract class Entity<TId>
+    {
+        public TId Id { get; }
+
+        public Entity(TId id)
+        {
+            Id = id;
+        }
+
+        protected bool Equals(Entity<TId> other)
+        {
+            return EqualityComparer<TId>.Default.Equals(Id, other.Id);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Entity<TId>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<TId>.Default.GetHashCode(Id);
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}({nameof(Id)}: {Id})";
+        }
+    }
+}
---
+++ Domain/Infrastructure/ValueType.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Domain.Infrastructure
+{
+    /// <summary>
+    /// Базовый класс типов-значений
+    /// </summary>
+    /// <typeparam name="T">Тип наследуещегося класса</typeparam>
+    public abstract class ValueType<T>
+    {
+        private static readonly PropertyInfo[] Properties =
+            typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        protected bool Equals(T other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            foreach (var propertyInfo in Properties)
+            {
+                var thisPropertyValue = propertyInfo.GetValue(this);
+                var otherPropertyValue = propertyInfo.GetValue(other);
+
+                if (!Equals(thisPropertyValue, otherPropertyValue)) return false;
+            }
+
+            return true;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals((T) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return string.Join(null, Properties.Select(prop => prop.GetValue(this)?.GetHashCode())).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            var formattedProps = string.Join(";",
+                Properties
+                    .OrderBy(p => p.Name)
+                    .Select(p => $"{p.Name}: {p.GetValue(this)}"));
+            return '(' + formattedProps + ')';
+        }
+    }
+}
